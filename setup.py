b7107cb59c3a3fe2a425acee0318abb4537d1dfa
koito19960406 <61777450+koito19960406@users.noreply.github.com>
2023-01-23 11:52:11 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A review paper on studies that used visual input to study urban perception',
+    author='Koichi Ito',
+    license='MIT',
+)
